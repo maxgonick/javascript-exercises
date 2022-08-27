9f9c6dc0a471c0a12ada37bd4489bd95486d6ab9
maxgonick <maxgonick@gmail.com>
2022-08-27 10:31:02 -0700

    sol tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (inputF) {
+  inputF = (inputF - 32) * (5 / 9);
+  return parseFloat(inputF.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (inputC) {
+  inputC = 1.8 * inputC + 32;
+  return parseFloat(inputC.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
