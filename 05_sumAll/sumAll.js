09f6eb7373069cb6a08cc1fb9101152047c03094
maxgonick <maxgonick@gmail.com>
2022-08-27 10:18:34 -0700

    sumAll sol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstInput, secondInput) {
+  let sum = 0;
+  let largerInput;
+  let smallerInput;
+  //input validation
+  if (typeof firstInput != "number" || typeof secondInput != "number") {
+    return "ERROR";
+  }
+  if (firstInput < 0 || secondInput < 0) {
+    return "ERROR";
+  }
 
+  if (firstInput > secondInput) {
+    largerInput = firstInput;
+    smallerInput = secondInput;
+  } else {
+    largerInput = secondInput;
+    smallerInput = firstInput;
+  }
+  for (let i = smallerInput; i <= largerInput; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
