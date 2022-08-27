8bb0220cffae116c234ae9b53e75959a65c697b5
maxgonick <maxgonick@gmail.com>
2022-08-26 18:42:09 -0700

    sol reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  const reversedArray = Array.from(input).reverse();
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
