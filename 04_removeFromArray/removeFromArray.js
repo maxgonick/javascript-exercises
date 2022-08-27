e092c655ba34ac2c722b098ce0235bcef95e9736
maxgonick <maxgonick@gmail.com>
2022-08-27 10:11:40 -0700

    sol removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...args) {
+  //Find the elements
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < inputArray.length; j++) {
+      if (inputArray[j] === args[i]) {
+        inputArray.splice(j, 1);
+        j = 0;
+        continue;
+      }
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
