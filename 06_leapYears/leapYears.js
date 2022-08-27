99e6972afcbda5ab4a90cb4a72214fceabeb1719
maxgonick <maxgonick@gmail.com>
2022-08-27 10:22:20 -0700

    leapYears sol

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (inputYear) {
+  if (inputYear % 4 == 0) {
+    if (inputYear % 100 != 0) {
+      return true;
+    } else {
+      if (inputYear % 400 == 0) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 // Do not edit below this line
