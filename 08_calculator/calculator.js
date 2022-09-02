bb9d7add4cd85ac2df827271c97014fe07639727
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-02 15:37:46 -0700

    08 calc sol

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (inputArray) {
+  return inputArray.reduce((total, next) => {
+    return total + next;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (inputArray) {
+  return inputArray.reduce((total, next) => {
+    return total * next;
+  });
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (input) {
+  if (input == 1) {
+    return 1;
+  }
+
+  if (input == 0) {
+    return 1;
+  }
+
+  return input * factorial(input - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
