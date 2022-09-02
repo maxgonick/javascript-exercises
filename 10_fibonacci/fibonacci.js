98686fa5dbb6077470b4f80b64331510f0495416
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-02 16:40:04 -0700

    10 fibo sol

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (index < 0) {
+    return "OOPS";
+  }
 
+  let firstIndex = 0;
+  let nextIndex = 1;
+
+  for (let i = 0; i < index; i++) {
+    let tempNext = nextIndex;
+    nextIndex = firstIndex + nextIndex;
+    firstIndex = tempNext;
+  }
+  return firstIndex;
 };
 
 // Do not edit below this line
