4bda6f8d9c8d84a1b3bfdc1363a1aacd837718f0
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-02 16:16:47 -0700

    09 palindromes sol

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+  //Cleaning up input string
+  inputString = inputString.toLowerCase();
+  inputString = inputString.replaceAll("!", "");
+  inputString = inputString.replaceAll(",", "");
+  inputString = inputString.replaceAll(".", "");
+  inputString = inputString.replaceAll(" ", "");
 
+  //Reverse string
+  const reverseString = inputString.split("").reverse().join("");
+  return reverseString == inputString ? true : false;
 };
 
 // Do not edit below this line
