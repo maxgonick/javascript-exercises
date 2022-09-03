a33df07bdc1c46941444c903d17758996a712108
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-02 18:56:25 -0700

    12 findTheOldest sol

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (objArray) {
+  const aliveArray = objArray.map((element) => {
+    let yearsAlive;
+    if (element.yearOfDeath == undefined) {
+      let currentYear = new Date().getFullYear();
+      return currentYear - element.yearOfBirth;
+    } else {
+      return element.yearOfDeath - element.yearOfBirth;
+    }
+  });
+  let longestAlive = aliveArray.reduce((accumulator, currentValue) => {
+    return Math.max(accumulator, currentValue);
+  });
 
+  let index = aliveArray.findIndex((element) => element == longestAlive);
+
+  return objArray[index];
 };
 
 // Do not edit below this line
